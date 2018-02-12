340abdc25afa305e8eaf5bd0e6d1289b4cf10d2a
kqarryzada <khalid.qarryzada@gmail.com>
2018-02-11 23:02:14 -0600

    Updated HOST to proper one for twitch

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-HOST = "irc.twitch.tv"
+HOST = "irc.chat.twitch.tv"
 PORT = 6667
-PASS = ""           # Get password from twitchapps.tv
+PASS = ""           # Get password from twitchapps.tv/tmi
 
 # Make sure these are lower case
 LOGIN_ID = ""       # Username of bot
