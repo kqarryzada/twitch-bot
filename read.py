b1bf40c408e882c04bde7989ecbe52772dbef85a
kqarryzada <khalid.qarryzada@gmail.com>
2018-02-12 01:10:09 -0600

    Added primitive bot functions of displaying chat and ponging to pings

---
+++ read.py
@@ -0,0 +1,16 @@
+import string
+
+def getUserAndMsg(line):
+    if line == "":
+        print "\n\n\n\nEmpty string entered into getUserAndMsg()\n\n\n\n"
+        return None
+
+    separate = line.split(":", 2)
+    seg = separate[1]
+    message = separate[2]
+
+    # seg now contains [username]![other junk], so extract
+    seg2 = seg.split("!", 1)
+    username = seg2[0]
+    
+    return username, message
--- settings.py
+++ settings.py
@@ -4,5 +4,4 @@ PASS = ""           # Get password from twitchapps.tv/tmi
 
 # Make sure these are lower case
 LOGIN_ID = ""       # Username of bot
-CHANNEL_ID = ""     # Username of chennel to join
-
+CHANNEL_ID = ""     # Username of channel to join
